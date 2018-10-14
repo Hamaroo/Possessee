35cc320acddfe6e46e55790a2186345e65d8df24
Hamaroo <slay3rofshad0w@gmail.com>
2018-10-14 12:18:26 -0700

    Root

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    public Transform target;
+    public float angularSpeed;
+
+    public Vector3 initialOffset;
+    private Vector3 currentOffset;
+
+    [ContextMenu("Set Current Offset")]
+    private void SetCurrentOffset() {
+        if (target == null) {
+            return;
+        }
+
+        initialOffset = transform.position - target.position;
+    }
+
+    private void Start() {
+        if (target == null) {
+            Debug.LogError("Assign a target for the camera in Unity's inspector");
+        }
+
+        currentOffset = initialOffset;
+    }
+
+    private void LateUpdate() {
+        transform.position = target.position + currentOffset;
+
+        float movement = Input.GetAxis("Alt Horizontal") * angularSpeed * Time.deltaTime;
+        if (!Mathf.Approximately(movement, 0f)) {
+            transform.RotateAround(target.position, Vector3.up, movement);
+            currentOffset = transform.position - target.position;
+        }
+    }
+}
---
+++ Assets/moving.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour {
+    Rigidbody rb;
+    public float speed = 10;
+    public float x, z;
+    public GameObject posessee;
+
+    public Transform cam;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponentInParent<Rigidbody>();
+        reconstructSoul();
+
+	}
+
+    void reconstructSoul() {
+        posessee = transform.parent.gameObject;
+        transform.localPosition = new Vector3(0, 0, 0);
+        rb = GetComponentInParent<Rigidbody>();
+        Debug.Log("switched");
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if(posessee != transform.parent.gameObject) {
+            reconstructSoul();
+        }
+
+        if (Input.GetAxisRaw("Change Camera") > 0) {
+            //switch to other camera
+
+        }
+        else {
+            float horiz = Input.GetAxis("Horizontal");
+            float verti = Input.GetAxis("Vertical");
+
+            x = horiz;
+            z = verti;
+
+            horiz *= speed;
+            verti *= speed;
+
+            Vector3 forward = cam.forward;
+            forward.y = 0;
+
+            Vector3 right = cam.right;
+            right.y = 0;
+
+            forward.Normalize();
+            right.Normalize();
+
+            Vector3 gravity = new Vector3(0, rb.velocity.y, 0);
+
+            Vector3 force = forward * verti + cam.right.normalized*horiz + gravity;
+            rb.velocity = force;
+        }
+        
+
+	}
+}
